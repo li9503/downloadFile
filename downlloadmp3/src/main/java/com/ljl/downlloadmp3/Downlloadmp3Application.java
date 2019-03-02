a57c7c74cf1e6af5493629e9e838d4a33071dbfb
li9503 <438903621@qq.com>
2019-03-02 22:24:55 +0800

    IDEA

---
+++ downlloadmp3/src/main/java/com/ljl/downlloadmp3/Downlloadmp3Application.java
@@ -0,0 +1,13 @@
+package com.ljl.downlloadmp3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Downlloadmp3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Downlloadmp3Application.class, args);
+	}
+
+}
